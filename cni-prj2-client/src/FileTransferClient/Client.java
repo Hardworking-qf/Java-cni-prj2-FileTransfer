a11a2beffc6cef7c75b236506fda251e75bc908a
Hardworking-qf <52162311+Hardworking-qf@users.noreply.github.com>
2020-04-06 16:15:52 +0800

    update

---
+++ cni-prj2-client/src/FileTransferClient/Client.java
@@ -0,0 +1,13 @@
+package FileTransferClient;
+import java.io.IOException;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+    	try {  
+            FileTransferClient client = new FileTransferClient();
+            client.SendFile("1.txt");
+        } catch (Exception e) {  
+            System.out.println("ÎÄ¼þ´«ÊäÊ§°Ü");
+        }  
+    }
+}
